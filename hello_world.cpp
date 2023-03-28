17259d4a24257a66e51b54c6202c0e310adc5487
kkukuruz <kkukuruzzk@mail.ru>
2023-03-29 00:28:04 +0300

    Add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,6 @@ int main()
 	string name;
 	cout<<"Enter your name:";
 	cin>>name;
-	cout<<"Hello world from @"<<name<<endl;
+	cout<<"Hello world from @"<<name<<endl;//Output the greeting message to the console
 	return 0;
 }
