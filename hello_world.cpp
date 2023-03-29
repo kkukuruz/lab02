ea89681e108cd991126f447fd31afd5a28d553d2
kkukuruz <kkukuruzzk@mail.ru>
2023-03-29 23:01:25 +0300

    Change code style using clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 
-int main()
+int
+main()
 {
+<<<<<<< HEAD
 	string name;
 	cout<<"Enter your name:";
 	cin>>name;
-	cout<<"Hello world from @"<<name<<endl;//Output the greeting message to the console; コンソールにグリーティング メッセージを出力する
+	cout<<"Hello world from @"<<name<<endl;//Output the greeting message to the console; new style
 	return 0;
+=======
+  string name;
+  cout << "Enter your name:";
+  cin >> name;
+  cout << "Hello world from @" << name
+       << endl; // Output the greeting message to the console
+  return 0;
+>>>>>>> 6788c5c (Change code style using clang-format)
 }
