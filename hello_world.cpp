fce89fa2aa73e3335cecea683574c1fd64bba667
kkukuruz <116309607+kkukuruz@users.noreply.github.com>
2023-04-27 20:29:19 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,16 +5,16 @@ main()
 {
 <<<<<<< HEAD
 	string name;
-	cout<<"Enter your name:";
-	cin>>name;
-	cout<<"Hello world from @"<<name<<endl;//Output the greeting message to the console; new style
+	std::cout<<"Enter your name:";
+	std::cin>>name;
+	std::cout<<"Hello world from @"<<name<<std::endl;//Output the greeting message to the console; new style
 	return 0;
 =======
   string name;
-  cout << "Enter your name:";
-  cin >> name;
-  cout << "Hello world from @" << name
-       << endl; // Output the greeting message to the console
+  std::cout << "Enter your name:";
+  std::cin >> name;
+  std::cout << "Hello world from @" << name
+       << std::endl; // Output the greeting message to the console
   return 0;
 >>>>>>> 6788c5c (Change code style using clang-format)
 }
